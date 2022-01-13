ffedb4d7c3954fcf35710afbd2b76bb582d6e206
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-01-13 15:04:22 -0800

    added addMovie component and replaced API url with firebase DB url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
     setError(null);
 
     try {
-      const response = await fetch('https://swapi.py4e.com/api/films');
+      const response = await fetch('https://react-api-f9e4d-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
@@ -40,6 +41,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  const addMovieHandler = movie => {
+    console.log(movie);
+  };
+
   let content = <p>Found no movies</p>;
 
   if (movies.length > 0) {
@@ -54,6 +59,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
